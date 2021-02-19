9e151f61cccabd7d59a06780ac1247a64c274e03
Brandon Martinez <brandonam04@outlook.com>
2021-02-19 07:22:52 -0500

    Add templates for registration form
    This commit adds basic templates for the user registration form. Pretty self explainatory.

--- dialedIn/dialedIn/settings.py
+++ dialedIn/dialedIn/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'dialedTimer.apps.DialedtimerConfig',
+    'dialedTimer.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
