1955f42526578ef6b7423c72ce57ec3c6bebff17
Brandon Martinez <brandonam04@outlook.com>
2021-02-19 11:32:59 -0500

    Style the Registration form with crispy forms

--- dialedIn/users/forms.py
+++ dialedIn/users/forms.py
@@ -1,10 +1,20 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_id = 'registrationFrom'
+        self.helper.form_class = 'blueForms'
+        self.helper.form_method = 'post'
+        self.helper.form_action = 'submit_survey'
 
+        self.helper.add_input(Submit('submit', 'Submit'))
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
