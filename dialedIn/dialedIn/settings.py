b3c862f2839d023332574013c45b9e7c28692b48
Brandon Martinez <brandonam04@outlook.com>
2021-02-19 08:09:35 -0500

    Add registration view
    This adds the view that processes the registration form and saves registered users to the db.
    Added success message to base template.
    TODO:
    	Proper bootstrap template
    	Get a navbar
    	Add admin page

--- dialedIn/dialedIn/settings.py
+++ dialedIn/dialedIn/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'dialedTimer.apps.DialedtimerConfig',
-    'dialedTimer.apps.UsersConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
