a45bdb71152e5d38d65a5c4eefd6e0a3a0c814f9
Skyward176 <brandonam04@gmail.com>
2021-02-20 20:07:52 -0500

    Fix coffee and extraction models, add logout page

--- dialedIn/dialedTimer/admin.py
+++ dialedIn/dialedTimer/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Coffees, Extractions
+from .models import Coffee, Extraction
 # Register your models here.
-admin.site.register(Coffees)
-admin.site.register(Extractions)
+admin.site.register(Coffee)
+admin.site.register(Extraction)
--- dialedIn/dialedTimer/models.py
+++ dialedIn/dialedTimer/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Coffees(models.Model):
+
+class Coffee(models.Model):
     coffee_name = models.CharField(max_length=30)
     roaster_name = models.CharField(max_length=30)
     roast_level = models.CharField(max_length=30)
-class Extractions(models.Model):
+
+
+class Extraction(models.Model):
     user_id = models.ForeignKey(User, on_delete = models.CASCADE)
-    coffee_id = models.ForeignKey(Coffees, on_delete = models.CASCADE)
+    coffee_id = models.ForeignKey(Coffee, on_delete = models.CASCADE)
     timestamp = models.DateTimeField()
     extraction_length = models.IntegerField()
     mass_in= models.DecimalField(max_digits=5, decimal_places=3)
     mass_out = models.DecimalField(max_digits=5, decimal_places=3)
     notes = models.CharField(max_length=300)
-
