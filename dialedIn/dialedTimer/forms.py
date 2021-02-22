67c907c1c74da101befa2ea8bc54d560195ec888
Brandon Martinez <brandonam04@outlook.com>
2021-02-21 19:31:02 -0500

    Add template for coffee form

--- dialedIn/dialedTimer/forms.py
+++ dialedIn/dialedTimer/forms.py
@@ -1,17 +1,6 @@
-from django import forms
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
-
-class CoffeeCreationForm(forms.Form):
-    coffee_name = forms.CharField()
-    roaster_name = forms.CharField()
-    roast_level = forms.CharField()
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.form_id = 'coffeeForm'
-        self.helper.form_class = 'blueForms'
-        self.helper.form_method = 'post'
-        self.helper.form_action = 'home'
-
-        self.helper.add_input(Submit('submit', 'Submit'))
+from django.forms import ModelForm
+from .models import Coffee
+class CoffeeCreationForm(ModelForm):
+    class Meta:
+        model = Coffee
+        fields = ['coffee_name', 'roaster_name', 'roast_level']
--- dialedIn/dialedTimer/views.py
+++ dialedIn/dialedTimer/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib import messages
+from . import forms as timer_forms
+
 
 def home(request):
-    return render(request,'dialedTimer/home.html')
+    if request.method == 'POST':
+        form = timer_forms.CoffeeCreationForm(request.POST)
+        if form.is_valid():
+            form.save()#the save method of usercreation form automagically saves our user accounts
+            coffee_name = form.cleaned_data.get('coffee_name')
+            messages.success(request, f'Coffee {coffee_name} was created!')
+
+            return redirect('home')
+    else:
+        form = timer_forms.CoffeeCreationForm()
+    return render(request, 'dialedTimer/home.html', {'form': form})
