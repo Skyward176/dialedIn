f93f9b4de1a48d9c87fbfe5ff90e8e65714d9eb9
Brandon Martinez <brandonam04@outlook.com>
2021-03-08 16:44:13 -0500

    Edit settings to config postgres db

--- dialedIn/dialedIn/settings.py
+++ dialedIn/dialedIn/settings.py
@@ -12,10 +12,17 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import django_heroku
+import dj_database_url
+import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# dotenv cofiguration for different production and testing dbs
+
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -77,13 +84,8 @@ WSGI_APPLICATION = 'dialedIn.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -128,3 +130,7 @@ LOGIN_REDIRECT_URL = "/"
 LOGIN_URL = "login"
 
 django_heroku.settings(locals())
+
+#fix ssl with local db
+options = DATABASES['default'].get('OPTIONS', {})
+options.pop('sslmodee', None)
