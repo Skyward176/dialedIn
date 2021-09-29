f1f41854ab2b3f56ad22e2e512b954be543e7351
Brandon Martinez <brandonam04@outlook.com>
2021-09-28 22:09:40 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,15 @@ echo "Docker container started"
 
 echo "SHELL=/bin/bash"
 
-gunicorn dialedIn.wsgi:application --bind 0.0.0.0:$PORT
+if [ -z "$SSH_CLIENT" ] && [ -n "$HEROKU_EXEC_URL" ];
+then
+    source <(curl --fail --retry 3 -sSL "$HEROKU_EXEC_URL")
+fi
+
+if [[ "$DYNO" =~ ^release.* ]];
+then
+    set -e
+    python3 manage.py migrate
+else
+    exec gunicorn teleagh.wsgi  -b 0.0.0.0:${PORT} --reload --access-logfile -
+fi
